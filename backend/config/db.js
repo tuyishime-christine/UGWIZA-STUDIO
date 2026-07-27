41abb868665fa2b020fe0b0616926e84aaf06b7d
tuyishime-christine <christinetuyishime67@gmail.com>
2026-07-27 18:01:04 +0300

    Updated db.js to include DB_PORT and detailed logging

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,14 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({ path: path.join(__dirname, '../.env') });
-
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_NAME:', process.env.DB_NAME);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
